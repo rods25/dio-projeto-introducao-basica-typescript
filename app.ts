61f18ca768dbbc4f5532ca6ee7f55a63048d0955
Rodrigo Ribeiro <rodrigo.rifesa@gmail.com>
2022-06-24 12:10:41 -0300

    Primeiro commit

---
+++ app.ts
@@ -0,0 +1,15 @@
+let button = document.getElementById('button'),
+input1 = document.getElementById('input1') as HTMLInputElement,
+input2 = document.getElementById('input2') as HTMLInputElement;
+
+function somarNumeros(num1: number,num2: number) {
+    return num1 + num2;
+}
+
+if(button) {
+    button.addEventListener('click', () => {
+        if(input1 && input2) {
+            console.log(somarNumeros(Number(input1.value),Number(input2.value)));
+        }
+    }); 
+}
